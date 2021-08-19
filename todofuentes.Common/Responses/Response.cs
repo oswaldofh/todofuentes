81dd4e5d8c5fad2ad0160b6163dc7302e137358f
oswaldofh <oswaldofuentes249734@correo.itm.edu.co>
2021-08-19 18:38:28 -0500

    add entity

---
+++ todofuentes.Common/Responses/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace todofuentes.Common.Response
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+
+    }
+}
---
+++ todofuentes.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todofuentes.Functions.Entities
+{
+    public class TodoEntity: TableEntity
+    {
+
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
